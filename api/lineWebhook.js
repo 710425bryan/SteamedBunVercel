5db1879fdcd36fa49ba4aa66b26aea94c39b3669
Bryan <>
2024-11-14 11:11:30 +0800

    add line webhook

---
+++ api/lineWebhook.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const line = require('@line/bot-sdk');
+
+const config = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+};
+
+const client = new line.Client(config);
+const app = express();
+
+// Line Webhook Endpoint
+app.post('/api/webhook', line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+function handleEvent(event) {
+  if (event.type === 'message' && event.message.type === 'text') {
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: `你說了: ${event.message.text}`,
+    });
+  } else {
+    return Promise.resolve(null);
+  }
+}
+
+module.exports = app;
