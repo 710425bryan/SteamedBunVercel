2547ff3ad1d8594523e6b24020470450b723cd79
Bryan <>
2024-11-14 15:13:04 +0800

    add next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self' https://vercel.live"
+          }
+        ]
+      }
+    ];
+  },
+};
