39af12148b2172603dc30e7ef7a126d21a567a1f
Bryan <>
2024-11-14 17:17:18 +0800

    fix firebase database url issue

--- api/firebase.js
+++ api/firebase.js
@@ -1,11 +1,12 @@
 const admin = require('firebase-admin');
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK_KEY);
+const firebaseURL = process.env.VITE_FIREBASE_DATABASE_URL;
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
+    databaseURL: firebaseURL,
   });
 }
 
