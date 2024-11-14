41952e6923fa5fa4fa7a28bf718aac4610152497
Bryan <>
2024-11-14 17:12:51 +0800

    fix: firebase This means you called initializeApp() more

--- api/firebase.js
+++ api/firebase.js
@@ -2,10 +2,13 @@ const admin = require('firebase-admin');
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK_KEY);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
+  });
+}
+
 
 const db = admin.database();
 
