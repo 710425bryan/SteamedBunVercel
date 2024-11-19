5a0623f236c926a977931b15ebe64371f4cc397e
Bryan <>
2024-11-19 13:47:07 +0800

    fix upload message log

--- api/messages.js
+++ api/messages.js
@@ -67,7 +67,7 @@ app.post('/api/markAsRead', async (req, res) => {
 app.post('/api/uploadImage', async (req, res) => {
   try {
     const { file } = req.body;
-    console.log('api/uploadImage file:', file)
+    console.log('api/uploadImage file:', req.body)
     if (!file) {
       return res.status(400).json({ error: 'Missing file' });
     }
